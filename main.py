e28403d43d6ce23eb27067ce1ec1daa32036b270
zakig7 <24729443+zakig7@users.noreply.github.com>
2025-11-16 16:43:17 +0100

    optimize cpu util

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -15,6 +17,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
