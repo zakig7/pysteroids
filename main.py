662a6ed47370fd2f6f3dcfc693b13efe26f59e51
zakig7 <24729443+zakig7@users.noreply.github.com>
2025-11-18 23:36:27 +0100

    Use Pygame group containers to avoid cluttering game loop

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # Use Pygame group containers to avoid cluttering game loop
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -17,13 +24,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:  
+            obj.draw(screen)
+
         pygame.display.flip()
 
-        # limit the framerate to 60 FPS
+        # limit framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
