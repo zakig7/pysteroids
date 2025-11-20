0b378431a1462840920a67e5850cad70719768bc
zakig7 <24729443+zakig7@users.noreply.github.com>
2025-11-20 01:28:19 +0100

    generate asteroidfield from astroid class

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
-    # Use Pygame group containers to avoid cluttering game loop
+    # Use Pygame sprite group containers
+    # to avoid manually adding new ones each time
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
     Player.containers = (updatable, drawable)
 
+    AsteroidField()
     Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
